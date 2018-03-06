96dfc430763b2a42d66144bea841f11a84170782
satwik <satwik.illini@gmail.com>
2018-03-05 22:15:35 -0600

    Fixed some issues

--- src/rhymz.py
+++ src/rhymz.py
@@ -3,26 +3,38 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
 url = 'http://www.nurseryrhymes.org/nursery-rhymes.html'
-response = urlopen(url)
-html = response.read()
-def getListOfUrl:
+
+#gets a list of all the urls from the landing page of rhymes website
+def getListOfUrl(url):
+    response = urlopen(url)
+    html = response.read()
     soup = BeautifulSoup(html, "html.parser")
     list = []
     for link in soup.find_all('a', href = True):
         list.append( link['href'])
     return list
 
+#filters the links of rhymes and fixes them by completing the links for bs4
 def fixLinks(links):
     fixedLinks = []
     for link in links:
-        if (!link.startswith('http')):
+        if (link.startswith('http') == False):
             fixedLinks.append("http://www.nurseryrhumes.org/" + link)
     return fixedLinks
 
+#gets the actual rhymes from the fixed links and adds them to a list
 def scrapeRhymes(links):
     rhymes = []
     for link in links:
-        soup = BeautifulSoup(link, "html.parser")
+        response = urlopen(link)
+        html = response.read()
+        soup = BeautifulSoup(html, "html.parser")
+        print(soup.find(id = "nursery-rhymes-lyrics").get_text())
         rhymes.append(soup.find(id = "nursery-rhymes-lyrics").get_text())
     return rhymes
+
+# call function
+rhymes = scrapeRhymes(fixLinks(getListOfUrl(url)))
+for text in rhymes:
+    print(text)
 
