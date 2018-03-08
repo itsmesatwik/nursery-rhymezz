81b272de6d70dd1ab12c5e8fb2c703d5543edfc8
satwik <satwik.illini@gmail.com>
2018-03-08 16:11:08 -0600

    Added scale for overall emotion

--- src/sentiment.py
+++ src/sentiment.py
@@ -12,5 +12,15 @@ def getScores():
 def calculateOverallSentiment(dict):
     sentimentWeight = 0
     for rhyme in dict:
-        sentimentWeight += dict[rhyme]["pos"] + dict[rhyme["neg"] + dict[rhyme]["neu"]
+        sentimentWeight += (dict[rhyme]["pos"] + dict[rhyme["neg"] + dict[rhyme]["neu"])
     return sentimentWeight
+
+def sentimentMeaning(weight):
+    if (weight > 0.15):
+        print("Rhyme is positive with a weight of " + weight)
+    elif (weight < -0.15):
+        print("Rhyme is dark with a weight of " + weight)
+    else:
+        print("Rhyme is neurtal with a weight of " + weight)
+
+    
