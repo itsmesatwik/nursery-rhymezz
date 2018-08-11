df113c00483e08612cfb6b2891b5131d3337fa67
satwik <satwik.illini@gmail.com>
2018-08-12 00:49:33 +0530

    Restarting project

--- src/rhymz.py
+++ src/rhymz.py
@@ -6,40 +6,27 @@ import os
 url = 'http://www.nurseryrhymes.org/nursery-rhymes.html'
 
 #gets a list of all the urls from the landing page of rhymes website
-def getListOfUrl(url):
-    response = urlopen(url)
-    html = response.read()
-    soup = BeautifulSoup(html, "html.parser")
-    list = []
-    for link in soup.find_all('a', href = True):
-        list.append( link['href'])
-    return list
+response = urlopen(url)
+html = response.read()
+soup = BeautifulSoup(html, "html.parser")
+list = []
+for link in soup.find_all('a', href = True):
+    list.append( link['href'])
 
 #filters the links of rhymes and fixes them by completing the links for bs4
-def fixLinks(links):
-    fixedLinks = []
-    for link in links:
-        if (link.startswith('http') == False):
-            fixedLinks.append("http://www.nurseryrhumes.org/" + link)
-    return fixedLinks
+fixedLinks = []
+for link in list:
+    if (link.startswith('http') == False):
+        fixedLinks.append("http://www.nurseryrhumes.org/" + link)
 
 #gets the actual rhymes from the fixed links and adds them to a list
-def scrapeRhymes(links):
-    rhymes = []
-    for link in links:
-        try:
-            response = urlopen(link)
-        except:
-            continue
-        html = response.read()
-        soup = BeautifulSoup(html, "html5lib")
-        print(soup.find(id = "nursery-rhymes-lyrics").get_text())
-        rhymes.append(soup.find(id = "nursery-rhymes-lyrics").get_text())
-    return rhymes
-
-# call function
-def printRhymes(rhymes):
-    for text in rhymes:
-        print(text)
-
-
+rhymes = []
+for link in links:
+    try:
+        response = urlopen(link)
+    except:
+        continue
+    html = response.read()
+    soup = BeautifulSoup(html, "html5lib")
+    print(soup.find(id = "nursery-rhymes-lyrics").get_text())
+    rhymes.append(soup.find(id = "nursery-rhymes-lyrics").get_text())
